52d602eff1b7ddf252b94858b327fecf0cbb4ec4
fathimathsafa <safahaneefa01@gmail.com>
2024-04-19 15:45:37 +0530

    payment page add

---
+++ lib/presentation/owner/payment_page.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:ration_shop/core/constants/color.dart';
+void main(){
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,
+  home:Payment_page(),));
+}
+
+class Payment_page extends StatefulWidget {
+
+
+  @override
+  State<Payment_page> createState() => _Payment_pageState();
+}
+
+class _Payment_pageState extends State<Payment_page> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorTheme.maincolor ,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 250),
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                child:  Lottie.asset("assets/Animation - 1711328118289 (1).json"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
